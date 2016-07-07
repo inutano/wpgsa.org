6b84ef70731bd4bd97e095b27438dee4ccf049d6
Tazro Inutano Ohta <inutano@gmail.com>
2016-07-07 14:03:07 +0900

    for fuckin ie

--- public/js/wpgsa/upload.js
+++ public/js/wpgsa/upload.js
@@ -4,12 +4,34 @@
 
 // onload
 $(function(){
+  // polyfill
+  pInclude();
+
   // enable filestyle
   $(":file").filestyle();
 
   // upload data
   uploadExpressionData();
 });
+
+// polyfill
+
+function pInclude(){
+  if (!String.prototype.includes) {
+    String.prototype.includes = function(search, start) {
+      'use strict';
+      if (typeof start !== 'number') {
+        start = 0;
+      }
+
+      if (start + search.length > this.length) {
+        return false;
+      } else {
+        return this.indexOf(search, start) !== -1;
+      }
+    };
+  }
+}
 
 // functions
 
