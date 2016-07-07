0b4315415b834888e9d57d0462def1cb6ac139cd
Tazro Inutano Ohta <inutano@gmail.com>
2016-07-07 13:47:50 +0900

    replace invalid characters

--- lib/wpgsa/docker.rb
+++ lib/wpgsa/docker.rb
@@ -31,7 +31,7 @@ module WPGSA
     end
 
     def staging_input_data(input_file) # return input file name
-      fname = input_file[:filename].encode('utf-8').gsub(/\s/,"_")
+      fname = input_file[:filename].encode('utf-8', :invalid => :replace, :undef => :replace ).gsub(/\s/,"_")
       open(File.join(@workdir, fname), "w"){|f| f.puts(input_file[:tempfile].read.encode('utf-8')) }
       fname
     end
